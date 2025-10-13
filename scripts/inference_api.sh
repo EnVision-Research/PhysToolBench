775d65368fa8bfc51ce957d79d92ec1c24ba4496
Zixin Zhang | 张梓欣 <zhangzixin0614@gmail.com>
2025-10-13 09:39:29 +0800

    update

--- scripts/inference_api.sh
+++ scripts/inference_api.sh
@@ -1,3 +1,1 @@
 python src/inference.py --model_name gpt-5 --api_url https://xxxxxx --api_key "sk-xxxxxx" --resume --cot_only --num_threads 1
-
-# python src/inference.py --model_name gpt-4o-mini --api_url http://43.139.170.244:9079/v1 --api_key "sk-jVOv32HLr4Q0qAcvBVWbiUJJMUqshjmdClV11XD1Mb7D1RlL" --mode cot_only --resume --num_threads 1
